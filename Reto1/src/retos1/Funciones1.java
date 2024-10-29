dc5e7a55263783d4276fa7354f873a7b53f1c36d
Lexitoelmastontito <alexfizquierdo10@gmail.com>
2024-10-29 10:00:52 +0100

    He creado la funcion de restar

--- Reto1/src/retos1/Funciones1.java
+++ Reto1/src/retos1/Funciones1.java
@@ -1,7 +1,11 @@
 package retos1;
 
 public class Funciones1 {
-	public static int sumar(int num1, int num2) {
+	public static int Sumar(int num1, int num2) {
 		return num1 + num2;
+	}
+	
+	public static int Restar(int num1, int num2) {
+		return num1 - num2;
 	}
 }
