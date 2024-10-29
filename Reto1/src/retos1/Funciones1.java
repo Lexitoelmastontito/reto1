2f5363b6c19f3d97bb2890db009c2c596210dbb4
Lexitoelmastontito <alexfizquierdo10@gmail.com>
2024-10-29 10:08:31 +0100

    He creado sumar,restar,esPar y esImpar

--- Reto1/src/retos1/Funciones1.java
+++ Reto1/src/retos1/Funciones1.java
@@ -8,4 +8,21 @@ public class Funciones1 {
 	public static int Restar(int num1, int num2) {
 		return num1 - num2;
 	}
+	public static boolean esPar(int num1) {
+		if(num1%2==0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public static boolean esImpar(int num1) {
+		if(num1%2==0) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+	
 }
