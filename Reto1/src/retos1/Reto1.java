4ae77a18e30fc7497bc6099088ca902c6484e124
Lexitoelmastontito <alexfizquierdo10@gmail.com>
2024-10-29 10:42:43 +0100

    Main creado con el menu interactivo y sus respectivas funciones

--- Reto1/src/retos1/Reto1.java
+++ Reto1/src/retos1/Reto1.java
@@ -1,12 +1,18 @@
 package retos1;
 
+import retos1.Funciones1.*;
+import retos1.Funciones2.*;
 import java.util.Scanner;
 
 public class Reto1 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner (System.in);
-		int n1=0;
+		Scanner sc = new Scanner(System.in);
+		int n1 = 0;
+		int n2 = 0;
+		int n3 = 0;
+		String s2 = "";
+		String s3 = "";
 		do {
 			System.out.println("Elige una operación:");
 			System.out.println("1. Sumar");
@@ -20,10 +26,76 @@ public class Reto1 {
 			System.out.println("0. Salir");
 			String s1 = sc.nextLine();
 			n1 = Integer.parseInt(s1);
-			
-			
-		}while(n1>=0);
-		
+
+			switch (n1) {
+
+			case 1:
+				System.out.println("Escribe el primer numero");
+				s2 = sc.nextLine();
+				n2 = Integer.parseInt(s2);
+				System.out.println("Escibe el segundo numero");
+				s3 = sc.nextLine();
+				n3 = Integer.parseInt(s3);
+				System.out.println(Funciones1.Sumar(n2, n3));
+				break;
+
+			case 2:
+
+				System.out.println("Escribe el primer numero");
+				s2 = sc.nextLine();
+				n2 = Integer.parseInt(s2);
+				System.out.println("Escibe el segundo numero");
+				s3 = sc.nextLine();
+				n3 = Integer.parseInt(s3);
+				System.out.println(Funciones1.Restar(n2, n3));
+				break;
+
+			case 3:
+
+				System.out.println("Escribe el primer numero");
+				s2 = sc.nextLine();
+				n2 = Integer.parseInt(s2);
+				System.out.println("Escibe el segundo numero");
+				s3 = sc.nextLine();
+				n3 = Integer.parseInt(s3);
+				System.out.println(Funciones2.multiplicacion(n2, n3));
+				break;
+			case 4:
+				System.out.println("Escribe el primer numero");
+				s2 = sc.nextLine();
+				n2 = Integer.parseInt(s2);
+				System.out.println("Escibe el segundo numero");
+				s3 = sc.nextLine();
+				n3 = Integer.parseInt(s3);
+				System.out.println(Funciones2.division(n2, n3));
+				break;
+			case 5:
+				System.out.println("Escribe el primer numero");
+				s2 = sc.nextLine();
+				n2 = Integer.parseInt(s2);
+				System.out.println(Funciones1.esPar(n2));
+				break;
+			case 6:
+				System.out.println("Escribe el primer numero");
+				s2 = sc.nextLine();
+				n2 = Integer.parseInt(s2);
+				System.out.println(Funciones1.esImpar(n2));
+				break;
+			case 7:
+				System.out.println("Escribe el primer numero");
+				s2 = sc.nextLine();
+				n2 = Integer.parseInt(s2);
+				System.out.println(Funciones2.esPositivo(n2));
+				break;
+			case 8:
+				System.out.println("Escribe el primer numero");
+				s2 = sc.nextLine();
+				n2 = Integer.parseInt(s2);
+				System.out.println(Funciones2.esNegativo(n2));
+				break;
+			}
+
+		} while (n1!=0);
 
 	}
 
