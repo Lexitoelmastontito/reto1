5ee5e0e71762efea90a04f8cfa1ddfb4083bd201
danielchuperca <ciuperciucd51@gmail.com>
2024-10-29 10:06:32 +0100

    Funciones

--- Reto1/src/retos1/Funciones2.java
+++ Reto1/src/retos1/Funciones2.java
@@ -1,12 +1,12 @@
 package retos1;
 
 public class Funciones2 {
-	public static int multiplicación(int num1, int num2) {
+	public static int multiplicacion(int num1, int num2) {
 		return (num1*num2);
 		
 		
 	}
-	public static int división(int num1, int num2) {
+	public static int division(int num1, int num2) {
 		if(num2==0) {
 			return 0;
 		}
